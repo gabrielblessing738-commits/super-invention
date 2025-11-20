608287c10737ce0c126772f961f94853903ab012
gabrielblessing738-commits <gabrielblessing738@gmail.com>
2025-11-20 23:17:47 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Fly.io without using a terminal!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
